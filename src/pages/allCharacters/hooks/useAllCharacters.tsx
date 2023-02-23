d68a299695c7dd33d18d7ed34e5530214b41ffcc
Aman Kumar Gupta <amang5840@gmail.com>
2023-02-23 23:23:09 +0530

    Add pagination

--- src/pages/allCharacters/hooks/useAllCharacters.tsx
+++ src/pages/allCharacters/hooks/useAllCharacters.tsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 
 import { type CharacterUI } from "types";
-import { useGetAllCharactersQuery } from "types/generated";
+import { type Info, useGetAllCharactersQuery } from "types/generated";
 
 import { getAllCharactersApi } from "../apis";
 
 export const useAllCharacters = () => {
 	const [allCharacters, setAllCharacters] = useState<CharacterUI[]>([]);
+	const [pageInfo, setPageInfo] = useState<Info>();
 
-	useGetAllCharactersQuery({
+	const getAllCharacterQuery = useGetAllCharactersQuery({
 		onError: (error) => {
 			console.log("error: ", error);
 		},
+		notifyOnNetworkStatusChange: true,
 		onCompleted: (res) => {
 			const dataFromApi = getAllCharactersApi(res);
+			setPageInfo(res.characters?.info ?? undefined);
 			setAllCharacters(dataFromApi);
 		},
 	});
 
-	return { allCharacters };
+	const handlePageChange = (page: number) => {
+		void getAllCharacterQuery.refetch({ page });
+	};
+
+	return {
+		allCharacters,
+		pageInfo,
+		loading: getAllCharacterQuery.loading,
+		handlePageChange,
+	};
 };
--- src/pages/allCharacters/ui/allCharacters.tsx
+++ src/pages/allCharacters/ui/allCharacters.tsx
@@ -1,15 +1,34 @@
-import { Box, Container } from "@mui/material";
+import {
+	Backdrop,
+	Box,
+	CircularProgress,
+	Container,
+	Pagination,
+} from "@mui/material";
 
 import { Navbar, CharacterCard } from "components";
 
 import { useAllCharacters } from "../hooks/useAllCharacters";
 
 export const AllCharacters = () => {
-	const { allCharacters } = useAllCharacters();
+	const { allCharacters, pageInfo, handlePageChange, loading } =
+		useAllCharacters();
 	return (
 		<>
 			<Navbar />
-			<Box display="flex" justifyContent="center">
+			<Backdrop
+				open={loading}
+				sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+			>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+			<Box
+				display="flex"
+				justifyContent="center"
+				overflow="scroll"
+				height="calc(100vh - 130px)"
+				marginBottom="12px"
+			>
 				<Container
 					sx={{
 						display: "flex",
@@ -30,6 +49,16 @@ export const AllCharacters = () => {
 					))}
 				</Container>
 			</Box>
+			<Container sx={{ display: "flex", justifyContent: "right" }}>
+				<Pagination
+					onChange={(e, page) => {
+						handlePageChange(page);
+					}}
+					count={pageInfo?.count ?? 0}
+					variant="outlined"
+					shape="rounded"
+				/>
+			</Container>
 		</>
 	);
 };
